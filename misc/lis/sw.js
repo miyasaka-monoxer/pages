bff55717090f8458d9ae44b80843b30852290b97
Miyasaka <miyasaka@monoxer.com>
2026-06-02 20:02:50 +0900

    add manifest files for lis-calculator

---
+++ misc/lis/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'lis-calculator-v1';
+const ASSETS = [
+  './lis-calculator.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
